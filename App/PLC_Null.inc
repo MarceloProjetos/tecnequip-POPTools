5b63575140b38d088840ba9ffc3029f42d23cce3
alessandro <alessandro@b4905fa6-274a-47ca-8038-d4c337c043bc>
2011-12-15 18:45:50 +0000

    Revertendo o último commit (142)

--- App/PLC_Null.inc
+++
@@ -1,6 +0,0 @@
-volatile int I_mcr = 0;
-
-void PLC_Run(void)
-{
-    I_mcr = 1;
-}
--- App/adc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ADC_H__
-#define __ADC_H__
-
-void ADC_Init(void);
-unsigned int ADC_Read(unsigned int a);
-
-#endif
-
