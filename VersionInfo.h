094cc060ffb85624e760e3b1809cf120ea111e98
alessandro <alessandro@b4905fa6-274a-47ca-8038-d4c337c043bc>
2012-02-07 10:39:09 +0000

    versão 1.2.991.5497 (branch 1.2)

--- VersionInfo.h
+++ VersionInfo.h
@@ -33,11 +33,11 @@
 //     since January 1, 2001 local time. The Y equals to the number of seconds since
 //     midnight local time, divided evenly by 10. The Z equals to the incremented build
 //     number and the letters A, B, C, D indicate that they remain unchanged.
-#define TEMPLATE "1.3.*"
+#define TEMPLATE "1.2.*"
 
 // Use this constant to display the version number.
-#define VERSION TEXT("1.3.991.4269")
+#define VERSION TEXT("1.2.991.5497")
 
 // Don't remove the line below. This number is incrementing by building process.
-#define BUILD 1394
+#define BUILD 1400
 
