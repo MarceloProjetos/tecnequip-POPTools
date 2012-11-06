e1fe778baab71efc8e9fbd6e1649c844a46da6d5
miranda <miranda@b4905fa6-274a-47ca-8038-d4c337c043bc>
2012-11-06 15:56:59 +0000

    + Adicionado os icones para as instrucoes ABS - POSICIONAMENTO - VALOR RANDOMICO - ESCRITA NO ENCODER corrigido #1114 + Adicionado um icone para o grupo encoder para permitir usar o recurso Size=Popup nos arquivos.xml ticket #1114

--- VersionInfo.h
+++ VersionInfo.h
@@ -36,8 +36,8 @@
 #define TEMPLATE "1.4.*"
 
 // Use this constant to display the version number.
-#define VERSION TEXT("1.4.1247.3974")
+#define VERSION TEXT("1.4.1265.4928")
 
 // Don't remove the line below. This number is incrementing by building process.
-#define BUILD 3820
+#define BUILD 3842
 
