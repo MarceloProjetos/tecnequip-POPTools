04ace422e4e0c9e5b3fcc0ee61606e0717764cb2
marcelo.fonseca <marcelo.fonseca@b4905fa6-274a-47ca-8038-d4c337c043bc>
2015-02-26 11:41:53 +0000

    + Adicionado comando ao Ribbon: Expansao

--- VersionInfo.h
+++ VersionInfo.h
@@ -36,8 +36,8 @@
 #define TEMPLATE "2.1.*"
 
 // Use this constant to display the version number.
-#define VERSION TEXT("2.1.2087.3128")
+#define VERSION TEXT("2.1.2107.2903")
 
 // Don't remove the line below. This number is incrementing by building process.
-#define BUILD 11073
+#define BUILD 11093
 
